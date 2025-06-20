45ca8cc1b91a079b7720f54f38c4f72d3bea460a
Nithesh Naredla <Nithesh.Naredla@coxautoinc.com>
2025-06-20 09:02:44 +0530

    c2c JWt

---
+++ jwt/JWT.BL/JwtAuthExtension.cs
@@ -0,0 +1,47 @@
+﻿using JWT.BL.Services;
+using JWT.BL.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JWT.BL
+{
+    public static class JwtAuthExtension
+    {
+        public static IServiceCollection AddServiceExtension(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.TryAddScoped<IJWTService, JWTService>();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var jwtKey = configuration["Jwt:Key"];
+                if (string.IsNullOrEmpty(jwtKey))
+                {
+                    throw new ArgumentNullException(nameof(configuration), "Jwt:Key configuration value cannot be null or empty.");
+                }
+
+
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                };
+            });
+            services.AddAuthorization();
+
+            return services;
+        }
+    }
+}
